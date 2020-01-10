3d21657bbd56ffbb7831122e4c6c4426b79b82f7
epiii <elfri.app@gmail.com>
2020-01-10 08:24:08 +0700

    Updates

--- precache-manifest.490980e5e822f80bb7277b13f00c99e4.js
+++ precache-manifest.490980e5e822f80bb7277b13f00c99e4.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5f457fef8f543edd29fab5c5c6adef3b",
+    "revision": "b6a1cc1f70103c12579f892594f807d2",
     "url": "/rtp/index.html"
   },
   {
-    "revision": "bea731c107ab85201165",
+    "revision": "218dbb3bcd528a2134d9",
     "url": "/rtp/static/css/2.c7b90ac4.chunk.css"
   },
   {
-    "revision": "d4585e95f528abfd04a8",
+    "revision": "6017ca79c3bc0a71fae5",
     "url": "/rtp/static/css/main.a12feb68.chunk.css"
   },
   {
-    "revision": "bea731c107ab85201165",
-    "url": "/rtp/static/js/2.abcde018.chunk.js"
+    "revision": "218dbb3bcd528a2134d9",
+    "url": "/rtp/static/js/2.bdcc1fd9.chunk.js"
   },
   {
     "revision": "87984cad984dae3222b2a1d0861003a4",
-    "url": "/rtp/static/js/2.abcde018.chunk.js.LICENSE"
+    "url": "/rtp/static/js/2.bdcc1fd9.chunk.js.LICENSE"
   },
   {
-    "revision": "d4585e95f528abfd04a8",
-    "url": "/rtp/static/js/main.79a569da.chunk.js"
+    "revision": "6017ca79c3bc0a71fae5",
+    "url": "/rtp/static/js/main.54bd38ec.chunk.js"
   },
   {
     "revision": "5b6ad0e04c7d38966fb7",
